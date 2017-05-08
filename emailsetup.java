6446463a839f41808e3f6b50de53a65ba6f948d3
Anon-E-Mus <bamdithya@gmail.com>
2017-05-08 11:33:53 +0530

    Add files via upload

---
+++ emailsetup.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class emailsetup
+{
+  public void (string emailID, string emailPW)  
+  {
+      
+    }
+    
+}
+
+class mane
+{
+public static void main(String args[])
+{
+    Scanner sc= new Scanner(System.in);
+    string info[]=new string ArrayList();
+    while(n!=3)
+    {
+       System.out.println("Enter 1 if you want to setup email and password \n Enter 2 if you want to access email  Enter 3 if you want to exit application");
+    int n= sc.nextInt();
+        switch(n)
+        {
+            case 1:
+            while(n!=0)
+            {
+                
+                 System.out.println("Enter the Email ID");
+                 string e= sc.nextLine();
+                 System.out.println("Enter the Password");
+                 string p= sc.nextLine();
+                 PrintWriter out= new PrintWriter(Email_ &_Password.txt);
+                 out.println(e);
+                 out.println(p);
+            
+    
+    
+    
